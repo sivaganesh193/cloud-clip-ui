662180a4a11ef63561e320909a3c9f686acb4ac0
saroja77 <sarojasubramanian77@gmail.com>
2024-09-25 23:58:06 -0400

    firebase update

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -5,25 +5,35 @@ import { Platform } from 'react-native';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
+const firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
+};
+
 // const firebaseConfig = {
-//     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-//     authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//     databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
-//     projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
+//     apiKey: "AIzaSyCLz2cD3T_LLmKNZr8I9y9eGL2bcn4Cc14",
+//     authDomain: "cloud-clip-d169d.firebaseapp.com",
+//     databaseURL: "https://cloud-clip-d169d-default-rtdb.firebaseio.com",
+//     projectId: "cloud-clip-d169d",
+//     storageBucket: "cloud-clip-d169d.appspot.com",
+//     messagingSenderId: "233163298991",
+//     appId: "1:233163298991:web:fa8764c35933ca02409c5a"
 // };
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCLz2cD3T_LLmKNZr8I9y9eGL2bcn4Cc14",
-    authDomain: "cloud-clip-d169d.firebaseapp.com",
-    databaseURL: "https://cloud-clip-d169d-default-rtdb.firebaseio.com",
-    projectId: "cloud-clip-d169d",
-    storageBucket: "cloud-clip-d169d.appspot.com",
-    messagingSenderId: "233163298991",
-    appId: "1:233163298991:web:fa8764c35933ca02409c5a"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAPTFzLjWbxwyE3-kUkGfV1-GsnK55gNxo",
+//     authDomain: "cloud-clip-us.firebaseapp.com",
+//     databaseURL: "https://cloud-clip-us-default-rtdb.firebaseio.com",
+//     projectId: "cloud-clip-us",
+//     storageBucket: "cloud-clip-us.appspot.com",
+//     messagingSenderId: "673044874369",
+//     appId: "1:673044874369:web:35a0e3ee39f18ab6e73980"
+//   };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
